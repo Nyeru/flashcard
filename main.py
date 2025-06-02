fd0e8e06f13bb77ff0b3e634619e169f6fa6455e
Long-Thanh Luong <zerozer0123@hotmail.com>
2025-06-02 11:35:15 +1000

    Variable numCard Input

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from FlashCard import Deck
 
 Deck1 = Deck()
-Deck1.addCards("Hiragana.csv")
-# Deck1.viewDeck()
-for i in range (5):
+csvName = "Hiragana"
+Deck1.addCards(csvName+".csv")
+print(f"{csvName} Deck Loaded.\n")
+numCards = "a"
+while numCards.isnumeric() is False:
+    numCards = input(f"Enter the amount of cards from {csvName} Deck to practice with:")
+    if numCards.isnumeric() is False:
+        print("Please input a number.")
+
+for i in range (int(numCards)):
     [front,back] = Deck1.randSel()
-    print(front)
-    input("Press Enter to reveal the back")
-    print(front, back)
+    print("Press Enter to reveal the back")
+    print(front, end='')
+    input("")
+    print(back)
