f821fc7a71d2c52fd75253e39420edc37e2b9f02
Long-Thanh Luong <zerozer0123@hotmail.com>
2025-07-11 20:49:46 +1000

    reorganised how variables are passed into html/jinja

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify, render_template
 from DeckActions import loadDeck, randomCards
 import json
-# TODO: List of availibe CSV, Dynamic Loading, Refreshing Gives new cards, Dynamic Number of Cards
-
+# TODO: Uploading own Deck, Either that or Availible Decks?
+# Uploading own deck, how will it work, will it replace the deck or append it to the current deck?
 app = Flask(__name__)
 
 deck = loadDeck("Hiragana")
@@ -12,8 +12,9 @@ def index():
         numCards = request.form.get("cardCount")
         print(numCards)
         cards = randomCards(deck, int(numCards))
-        return render_template('index.html', cards = cards, currDeck = deck.name, maxNumCards = deck.numCards)
-    return render_template("index.html",  currDeck = deck.name, maxNumCards = deck.numCards)
+        # Context Dictionary if need to pass many many variables!
+        return render_template('index.html', cards = cards, currDeck = deck)
+    return render_template("index.html",  currDeck = deck)
 
 
 if __name__ == '__main__':
