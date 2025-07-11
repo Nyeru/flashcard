4f9013f9901a1edb189d030f99152e23675fe339
Long-Thanh Luong <zerozer0123@hotmail.com>
2025-07-11 12:15:53 +1000

    functional WebApp, TODO: case when n >Deck Length

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask, request, jsonify, render_template
 from DeckActions import loadDeck, randomCards
+import json
+# TODO: List of availibe CSV, Dynamic Loading, Refreshing Gives new cards, Dynamic Number of Cards
 
 app = Flask(__name__)
 
-deck = loadDeck("hiragana")
-@app.route('/')
+deck = loadDeck("Hiragana")
+@app.route('/', methods=['POST', 'GET'])
 def index():
+    if request.method== "POST":
+        numCards = request.form.get("cardCount")
+        print(numCards)
+        cards = randomCards(deck, int(numCards))
+        return render_template('index.html', cards = cards)
     return render_template("index.html")
-@app.route('/cards', methods=['GET'])
-def serve_cards():
-    num = request.args.get('n', default=5, type=int)
-    cards = randomCards(deck, num)
-    print("Working")
-    return jsonify(cards)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
